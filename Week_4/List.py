f8b00d0ab8b841fab0587881c3789b3d4237e070
ShashankPatil20 <patil.shashankssp@gmail.com>
2020-05-11 18:56:19 +0530

    Examples on List

---
+++ Week_4/List.py
@@ -0,0 +1,15 @@
+fruits = ["Pineapple", "Banana", "Apple", "Orange"]
+fruits.append("Kiwi")
+print(fruits)
+
+fruits.insert(0, "Strawberry")
+print(fruits)
+
+fruits.remove("Strawberry")
+print(fruits)
+
+print(fruits.pop(0))
+print(fruits)
+
+fruits[0] = "Mango"
+print(fruits)
