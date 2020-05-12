1642864e052f3b9fe4d24f3325f90c4883ab3606
ShashankPatil20 <patil.shashankssp@gmail.com>
2020-05-12 20:47:34 +0530

    Examples on Class

---
+++ Week_5/Class.py
@@ -0,0 +1,19 @@
+class Apple:
+    color = ""
+    flavour = ""
+
+janegold = Apple()
+
+janegold.color = "Red"
+janegold.flavour = "Sweet"
+
+print(janegold.flavour.upper())
+
+class Flower:
+    color = ""
+
+Rose = Flower()
+Rose.color = "Red"
+
+print("Roses are {}".format(Rose.color))
+
